71279b2df1cd29805ed137fcef94bf1c64046e74
ozgurbaltaci <wozgurbaltaci@gmail.com>
2025-03-02 18:07:00 +0100

    Changes about responsiveness

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -14,6 +14,7 @@ import bootstrapIcon from "../SkillPictures/bootstrap.svg";
 import javaIcon from "../SkillPictures/java.png";
 import reduxIcon from "../SkillPictures/redux.png";
 import graphqlIcon from "../SkillPictures/graphql.png";
+import { HiOutlineLink } from "react-icons/hi2";
 
 
 import { Tooltip } from "react-tooltip";
@@ -58,20 +59,15 @@ const experiences = [
       {
         name: "Full-stack Developer Erasmus+ Intern",
         company: "Proceedit",
+        linkedin: "https://www.linkedin.com/company/proceedit-the-bpm-process'%E2%80%8B-factory-/posts/?feedView=all",
+        place: "Barcelona, Spain",
         workdates: "01/2025 - Present",
         technologies_used: "Flutterflow, Figma, GraphQL, Flask",
-        desc: `
-          <ul style="padding: 0; ">
-            <li style="margin-bottom: 5px;">
-             Experienced in FlutterFlow for fully responsive web/mobile development. Learning GraphQL & Flask to enhance backend capabilities.
-            </li>
-            <li style="margin-bottom: 5px;">
-              Skilled in Figma to create responsive, user-friendly designs with a focus on seamless UX.
-            </li>
-            <li style="margin-bottom: 5px;">
-              Proficient in Jira, collaborating in Agile teams for efficient development and problem-solving.
-            </li>
-          </ul>`,
+        desc: [
+          "Experienced in FlutterFlow for fully responsive web/mobile development. Learning GraphQL & Flask to enhance backend capabilities.",
+          "Skilled in Figma to create responsive, user-friendly designs with a focus on seamless UX.",
+          "Proficient in Jira, collaborating in Agile teams for efficient development and problem-solving."
+        ],
       },
     ],
   },
@@ -80,30 +76,18 @@ const experiences = [
     works: [
       {
         name: "Part-time Front-end Developer",
-        company: "Turkish Aerospace",
+        company: "Turkish Aerospace (TUSAS)",
+        linkedin: "https://www.linkedin.com/company/turkishaerospace/posts/?feedView=all",
+
+        place: "Antalya, Türkiye",
+
         workdates: "12/2022 - 02/2023",
         technologies_used: "ReactJS, MaterialUI, NodeJS, PostgreSQL",
-        desc: `
-          <ul style="padding: 0; ">
-            <li style="margin-bottom: 5px;">
-              Achieved 60% growth for a responsive and user-friendly dashboard
-              using React JS and Material UI skills.
-            </li>
-            <li style="margin-bottom: 5px;">
-              Developed comprehensive user requirement and use case documents
-              using effective communication and thorough documentation skills.
-            </li>
-            <li style="margin-bottom: 5px;">
-              Led front-end development initiatives, resulting in a 100%
-              improvement in user interface responsiveness and overall user
-              experience.
-            </li>
-            <li>
-              Contributed to Agile and Scrum methodologies, ensuring efficient
-              project delivery throughout the Software Development Life Cycle
-              (SDLC).
-            </li>
-          </ul>`,
+        desc: [
+          "Achieved 60% growth for a responsive and user-friendly dashboard using ReactJS, and MaterialUI skills.",
+          "Contributed to front-end development, achieving a 100% improvement in user interface responsiveness and overall user experience.",
+          "Contributed to Agile and Scrum methodologies, ensuring efficient project delivery throughout the Software Development Life Cycle (SDLC)."
+        ],
       },
     ],
   },
@@ -113,20 +97,16 @@ const experiences = [
       {
         name: "Intern Software Developer - Long Term",
         company: "Turkish Aerospace (TUSAS)",
+        linkedin: "https://www.linkedin.com/company/turkishaerospace/posts/?feedView=all",
+
+        place: "Antalya, Türkiye",
         workdates: "12/2021 - 07/2022",
         technologies_used: "ReactJS, Bootstrap, Express JS, Next JS",
-        desc: `
-          <ul style="padding: 0; ">
-            <li style="margin-bottom: 5px;">
-              Achieved 85% growth for a comprehensive, responsive and user-friendly Data Management System using React JS, Bootstrap, Express JS, and Next JS skills.
-            </li>
-            <li style="margin-bottom: 5px;">
-              Developed comprehensive user requirement and use case documents using effective communication and thorough documentation skills.
-            </li>
-            <li>
-              Contributed to Agile and Scrum methodologies, ensuring efficient project delivery throughout the Software Development Life Cycle (SDLC).
-            </li>
-          </ul>`,
+        desc: [
+          "Achieved 85% growth for an comprehensive, responsive and user-friendly Data Management System using ReactJS, Bootstrap, ExpressJS and NextJS skills.",
+          "Developed comprehensive user requirement and use case documents for 2 projects, improving team alignment and reducing revisions by 70% through effective communication and thorough documentation skills",
+          "Contributed to Agile and Scrum methodologies, ensuring efficient project delivery throughout the Software Development Life Cycle (SDLC)."
+        ],
       },
     ],
   },
@@ -136,20 +116,14 @@ const experiences = [
       {
         name: "Volunteer Software Developer Intern",
         company: "Jeli AI",
+        linkedin: "https://www.linkedin.com/company/jeliai/",
+        place: "Antalya, Türkiye",
         workdates: "10/2021 - 12/2021",
         technologies_used: "Vue JS",
-        desc: `
-          <ul style="padding: 0; ">
-            <li style="margin-bottom: 5px;">
-              Developed foundational skills in front-end development using Vue JS during my amateur phase.
-            </li>
-            <li style="margin-bottom: 5px;">
-              Created essential components such as footers and card holders to gain practical experience.
-            </li>
-            <li>
-              Actively contributed to collaborative teamwork, learning and growing as an aspiring developer during this period.
-            </li>
-          </ul>`,
+        desc: [
+          "Developed foundational skills in front-end development by building 10+ interactive web components using VueJS, gaining hands-on experience and improving proficiency in JavaScript frameworks during my early stage of learning.",
+
+        ],
       },
     ],
   },
@@ -173,29 +147,17 @@ const Skills = () => {
       </h2>
 
       <div className="skills-and-experiences">
-        {" "}
         <div className="skills-container">
-          <Grid container className="skills-grid">
-            {skills.map((item, index) => (
-              <Grid
-                className="skills-list"
-                item
-                xs={12}
-                sm={6}
-                md={4}
-                lg={3}
-                key={index}
-              >
-                <div className="skills-item">
-                  <div style={{ backgroundColor: item.backgroundColor }}>
-                    <img src={item.skillIcon} alt={item.skillName} />
-                  </div>
-                  <p>{item.skillName}</p>
-                </div>
-              </Grid>
-            ))}
-          </Grid>
-        </div>{" "}
+          {skills.map((item, index) => (
+            <div className="skills-item" key={index}>
+              <div className="skill-icon" style={{ backgroundColor: item.backgroundColor }}>
+                <img src={item.skillIcon} alt={item.skillName} />
+              </div>
+              <p>{item.skillName}</p>
+            </div>
+          ))}
+        </div>
+
         <div className="experiences-container">
           <h3 className="experiences-title">Experiences</h3>
           <div className="experiences-list">
@@ -211,19 +173,41 @@ const Skills = () => {
                       <div
                         className="app__skills-exp-work"
                         key={work.name}
-                        data-tooltip-id={work.desc ? "my-tooltip" : undefined}
-                        data-tooltip-html={`<div>${work.desc}</div>`}
+
                       >
                         <div style={{ width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+
                           <h5 className="skills_company_name">{work.name}</h5>
 
-                          <p style={{ fontSize: "12px" }}>{work.workdates}</p>
+
+
+                          <p style={{ fontSize: "11px" }}>{work.workdates}</p>
                         </div>
-                        <p>{work.company}</p>
 
 
+                        <div style={{ fontSize: "11px", fontStyle: "italic", width: "100%", display: "flex", justifyContent: "space-between", alignItems: "center" }}>
 
-                        <p style={{ fontSize: "12px" }}>{work.technologies_used}</p>
+                          <a href={work.linkedin} target="_blank" style={{ textDecoration: "none", color: "#000000" }}>
+                            <div style={{ display: "flex", alignItems: "center", gap: "3px" }}>
+                              <p>{work.company}</p>
+                              <HiOutlineLink />
+                            </div>
+                          </a>
+                          <p>{work.place}</p>
+
+                        </div>
+
+                        <div className="app__skills-exp-work-list">
+                          <ul style={{ padding: 0 }}>
+                            {work.desc.map((item, index) => (
+                              <li key={index} style={{ marginBottom: "5px" }}>
+                                {item}
+                              </li>
+                            ))}
+                          </ul>
+
+                        </div>
+
                       </div>
                     ))}
                   </div>
